cc68c3c3a8a5759451c1f1a45a34b852a820fd90
Smit Agola <smitagola675@gmail.com>
2025-01-12 11:56:08 +0530

    adding build option

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const db = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'todo',
+//     password: 'password',
+//     port: 5432,
+// })
+
+// export default db;
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const db = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'todo',
-    password: 'password',
-    port: 5432,
-})
+  connectionString: 'postgresql://crud_app_dnoz_user:mh38AzxnTTZpdE8PuqkIyN0oxzz2SvEE@dpg-ctl2bfbv2p9s738crjmg-a.oregon-postgres.render.com/crud_app_dnoz',
+  ssl: {
+    rejectUnauthorized: false, // This is typically required for Render
+  },
+});
 
 export default db;
