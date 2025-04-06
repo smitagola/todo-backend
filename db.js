c430fcba559edcf418afd368d16d3eb55d02bc6e
Smit Agola <smitagola675@gmail.com>
2025-04-06 10:40:13 +0530

    change in database password

--- db.js
+++ db.js
@@ -2,10 +2,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const db = new Pool({
-  connectionString: 'postgresql://postgres:SMIT@agola1208@db.pmrzzfsgeajjcohqmesg.supabase.co:5432/postgres',
-  ssl: {
-    rejectUnauthorized: false, // Required for Supabase connections
-  },
+  connectionString: 'postgresql://postgres:QjbbbGMU2IVfqOH0@db.pmrzzfsgeajjcohqmesg.supabase.co:5432/postgres',
+  ssl: false,
 });
 
 export default db;
